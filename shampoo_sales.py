8b2847089dd6a078c28229a02ab04732394620f6
Leonardo Angellotti <91626949+leonardoangellotti@users.noreply.github.com>
2021-12-05 13:29:53 +0000

    gestione di alcuni errori

--- shampoo_sales.py
+++ shampoo_sales.py
@@ -1,4 +1,3 @@
-
 #classe CVS file
 class FileCSV():
 
@@ -14,7 +13,7 @@ class FileCSV():
         print('tipo di errore: \n{}'.format(err))
 
   #prende data e sales e li mette in una lista
-  def get_data(self):
+  def get_data(self, start = None, end = None):
 
     my_file = open(self.name, 'r')
 
@@ -24,10 +23,11 @@ class FileCSV():
       line = lines.split(',')
 
       lista.append(line)
-
+      
     my_file.close()
 
-    print(lista)
+    #stampa lunghezza lista voluta
+    print(lista[start:end])
   
   #somma tutti i valori sales
   def sum_sales(self):
@@ -39,11 +39,21 @@ class FileCSV():
     for line in my_file:
       elements = line.split(',')
 
+      #salta la prima riga
       if elements[0] != 'Date':
+          
+        #prende il primo elemento: data, secondo elemento: sales
         date = elements[0]
         sales = elements[1]
 
-        lista_sum.append(float(sales))
+        try:
+            lista_sum.append(float(sales))
+        except:
+            if isinstance(sales, str) or sales == None:
+                print('converto il valore {} in 0.0, errore di conversione in float'.format(sales))
+                sales = 0.0
+                lista_sum.append(float(sales))
+
 
     for elements in lista_sum:
       print(elements)
@@ -57,6 +67,7 @@ class FileCSV():
 
     my_file.close()
 
+#classe converte tutte le colonen tranne la prima in float
 class NumericalCSVFile(FileCSV):
 
     def convert(self):
@@ -68,22 +79,27 @@ class NumericalCSVFile(FileCSV):
         for lines in my_file:
             line = lines.split(',')
 
+            #prende tutte le colonne tranne la prima
             except_the_1_column = line[1:]
 
+            #le aggiunge formato float a lista
             try: 
                 for item in except_the_1_column:
                     lista.append(float(item))
 
-
+            #errore
             except ValueError as err:
                 print("\nc'è stato un errore nella conversione")
                 print('tipo di errore trovato:\n{}'.format(err))
                 print('converto il valore in float 0.0\n')
+                
                 for item in except_the_1_column:
                     print('{} convertito in: '.format(item))
                     if type(item) != 'int' or 'float':
                         item = 0.0
                     print(item)
+                    lista.append(float(item))
+
 
             except Exception as err:
                 print("\nc'è stato un errore nella conversione")
@@ -100,6 +116,6 @@ class NumericalCSVFile(FileCSV):
 file = NumericalCSVFile('shampoo_sales.csv')
 
 #richiamo le funzioni definite
-file.get_data()
+file.get_data(20,30)
 file.sum_sales()
 file.convert()
