07a94e09b4167ce5368abe4488f6b3ef50e16afe
Leonardo Angellotti <91626949+leonardoangellotti@users.noreply.github.com>
2021-12-05 12:00:24 +0000

    NumericalCSVFile

--- shampoo_sales.py
+++ shampoo_sales.py
@@ -1,10 +1,19 @@
 
+#classe CVS file
 class FileCSV():
 
+  #inizializzo
   def __init__(self, name):
 
     self.name = name
 
+    try:
+        my_file = open(self.name, 'r')
+    except Exception as err:
+        print('il file non è leggibile')
+        print('tipo di errore: \n{}'.format(err))
+
+  #prende data e sales e li mette in una lista
   def get_data(self):
 
     my_file = open(self.name, 'r')
@@ -20,6 +29,7 @@ class FileCSV():
 
     print(lista)
   
+  #somma tutti i valori sales
   def sum_sales(self):
 
     my_file = open(self.name, 'r')
@@ -47,8 +57,49 @@ class FileCSV():
 
     my_file.close()
 
+class NumericalCSVFile(FileCSV):
 
-file = FileCSV('shampoo_sales.csv')
+    def convert(self):
+        
+        my_file = open(self.name, 'r')
 
+        lista = []
+
+        for lines in my_file:
+            line = lines.split(',')
+
+            except_the_1_column = line[1:]
+
+            try: 
+                for item in except_the_1_column:
+                    lista.append(float(item))
+
+
+            except ValueError as err:
+                print("\nc'è stato un errore nella conversione")
+                print('tipo di errore trovato:\n{}'.format(err))
+                print('converto il valore in float 0.0\n')
+                for item in except_the_1_column:
+                    print('{} convertito in: '.format(item))
+                    if type(item) != 'int' or 'float':
+                        item = 0.0
+                    print(item)
+
+            except Exception as err:
+                print("\nc'è stato un errore nella conversione")
+                print('tipo di errore trovato:\n{}\n'.format(err))
+
+
+        my_file.close()
+
+        print(lista)
+
+
+
+#attribuisco alla classe FileCSV il file shampoo_sales.csv 
+file = NumericalCSVFile('shampoo_sales.csv')
+
+#richiamo le funzioni definite
 file.get_data()
 file.sum_sales()
+file.convert()
