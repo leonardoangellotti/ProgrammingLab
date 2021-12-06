82960c5e58ecced117121207b5046af2241bfa19
Leonardo Angellotti <91626949+leonardoangellotti@users.noreply.github.com>
2021-12-06 17:13:00 +0000

    commenti, nuovi test

--- shampoo_sales.py
+++ shampoo_sales.py
@@ -6,8 +6,10 @@ class FileCSV():
 
     self.name = name
 
+    #cerca se c'è un file da aprire
     try:
         my_file = open(self.name, 'r')
+    #se non c'è restituisce l'errore
     except Exception as err:
         print('il file non è leggibile')
         print('tipo di errore: \n{}'.format(err))
@@ -15,15 +17,18 @@ class FileCSV():
   #prende data e sales e li mette in una lista
   def get_data(self, start = None, end = None):
 
+    #apre il file 
     my_file = open(self.name, 'r')
 
     lista = []
 
+    #divide gli elementi nel file e li aggiune ad una lista
     for lines in my_file:
-      line = lines.split(',')
+        line = lines.split(',')
 
-      lista.append(line)
-      
+        lista.append(line)
+        
+    #chiude il file
     my_file.close()
 
     #stampa lunghezza lista voluta
@@ -36,6 +41,7 @@ class FileCSV():
 
     lista_sum = []
 
+    #legge le linee del file
     for line in my_file:
       elements = line.split(',')
 
@@ -46,25 +52,33 @@ class FileCSV():
         date = elements[0]
         sales = elements[1]
 
+        #aggiunge alla lista i valori sales convertii in float
         try:
             lista_sum.append(float(sales))
+        #se non li può convertire li setta 0.0
         except:
             if isinstance(sales, str) or sales == None:
                 print('converto il valore {} in 0.0, errore di conversione in float'.format(sales))
                 sales = 0.0
                 lista_sum.append(float(sales))
 
-
+    #stampa tutti gli elementi nella lista
     for elements in lista_sum:
       print(elements)
 
+    #variabile somma inizializzata a 0.0
     somma = 0.0
 
+    #conferisce a var. somma la somma di tutti gli elementi della lista
     for elements in lista_sum:
       somma += elements
 
+    #stampa la somma
     print('\nla somma è {}'.format(round(somma,1)))
 
+    return somma
+
+    #chiude il file in modalità lettura
     my_file.close()
 
 #classe converte tutte le colonen tranne la prima in float
@@ -72,10 +86,12 @@ class NumericalCSVFile(FileCSV):
 
     def convert(self):
         
+        #apre il file in lettura
         my_file = open(self.name, 'r')
 
         lista = []
 
+        #legge ogni riga del file e ne fa lo split
         for lines in my_file:
             line = lines.split(',')
 
@@ -87,28 +103,34 @@ class NumericalCSVFile(FileCSV):
                 for item in except_the_1_column:
                     lista.append(float(item))
 
-            #errore
+            #se è un errore di tipo value
             except ValueError as err:
                 print("\nc'è stato un errore nella conversione")
                 print('tipo di errore trovato:\n{}'.format(err))
                 print('converto il valore in float 0.0\n')
                 
+                #per ogni elemento nelle successive colonne...
                 for item in except_the_1_column:
                     print('{} convertito in: '.format(item))
+                    #...se non è un intero o float lo setta a 0.0
                     if type(item) != 'int' or 'float':
                         item = 0.0
                     print(item)
+                    #aggiunge in una lista
                     lista.append(float(item))
 
-
+            #se è un'altro tipo di errore
             except Exception as err:
                 print("\nc'è stato un errore nella conversione")
                 print('tipo di errore trovato:\n{}\n'.format(err))
 
-
+        #chiude il file in lettura
         my_file.close()
 
+        #stampa la lista
         print(lista)
+
+        return lista
 
 
 
--- test_shampoo_sales.py
+++ test_shampoo_sales.py
@@ -1,10 +1,29 @@
 import unittest
 from shampoo_sales import FileCSV
+from shampoo_sales import NumericalCSVFile
 
 class TestCSVFIle(unittest.TestCase):
 
+    #guarda se il nome del file è shampoo_sales.csv
     def test_init(self):
 
         csv_file = FileCSV('shampoo_sales.csv')
 
-        self.assertEqual(csv_file.self,'shampoo_sales.csv')
+        self.assertEqual(csv_file.name,'shampoo_sales.csv')
+
+    #guarda se la somma è maggiore di 0
+    def test_get_data(self):
+
+        csv_file = FileCSV('shampoo_sales.csv')
+
+        csv_file.sum_sales() > 0
+
+    #guarda se la conversione è di tipo float
+    def test_convert(self):
+
+        csv_file = NumericalCSVFile('shampoo_sales.csv')
+
+        self.assertEqual(csv_file.convert(), float)
+
+        
+
